Fix: Remove double-scaling bug in humanoid_mimic reward logging
The logging was multiplying episode_sums by reward_scales again:
- episode_sums already contains (raw * scale * dt) from compute_reward()
- Logging was doing: episode_sums * reward_scales = raw * scale² * dt²
- Now just: episode_sums = raw * scale * dt (correct)

This bug affected all training runs but was most noticeable in V7 due to larger scales.

twist2/legged_gym/legged_gym/envs/base/humanoid_mimic.py
@@ -233,8 +233,10 @@ class HumanoidMimic(HumanoidChar):
         # fill extras
         self.extras["episode"] = {}
         for key in self.episode_sums.keys():
+            # episode_sums already contains scaled rewards (raw * scale * dt from compute_reward)
+            # Don't multiply by reward_scales again - that was a bug causing double-scaling!
             self.extras["episode"]['metric_' + key] = torch.mean(self.episode_sums[key][env_ids] / self._motion_lib.get_motion_length(self._motion_ids[env_ids]))
-            self.extras["episode"]['rew_' + key] = torch.mean(self.episode_sums[key][env_ids] * self.reward_scales[key] / self._motion_lib.get_motion_length(self._motion_ids[env_ids]))
+            self.extras["episode"]['rew_' + key] = torch.mean(self.episode_sums[key][env_ids] / self._motion_lib.get_motion_length(self._motion_ids[env_ids]))
             self.episode_sums[key][env_ids] = 0.
         
         for key in self.episode_means.keys():